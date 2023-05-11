d3951248b173fc3a8fcf819b7701762628cd9e99
MrChelizt <chelizt@gmail.com>
2023-05-11 14:24:40 +0200

    Refactor files, NavBar add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import ProductDetails from "./components/ProductDetails";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:productId" element={<ProductDetails />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+import Logo from "../screenshots/watermelon.png";
+
+export default function NavBar() {
+  return (
+    <div className="navBar">
+      <Link to="/">
+        <img src={Logo} alt="watermelon" />
+      </Link>
+      <Link to="/">
+        <div>Home</div>
+      </Link>
+      <Link to="/products">
+        <div>Products</div>
+      </Link>
+    </div>
+  );
+}
